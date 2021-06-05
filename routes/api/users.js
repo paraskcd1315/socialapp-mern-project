902f45916470d504a2574f7ff8b383b6f281f78e
Paras Khanchandani <paras_theloveable@hotmail.com>
2021-06-05 18:32:35 +0530

    jwt working

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const commonPasswords = [
@@ -84,8 +86,20 @@ router.post(
       await user.save();
 
       // Return jsonwebtoken
-
-      return res.json({ success: true });
+      const payload = {
+        user: {
+          id: user.id
+        }
+      };
+      jwt.sign(
+        payload,
+        config.get('jwtToken'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       return res.status(500).send('Server error');
